7cea7a8958c25368d369eaefb4fee629cd32ebe2
ChristosIl <el20233@mail.ntua.gr>
2024-12-08 15:19:27 +0200

    getTollStationPasses
    implemented the getTollStationPasses and tested with Postman. It looks functional

--- back-end/src/controllers/services.controller.ts
+++ back-end/src/controllers/services.controller.ts
@@ -1,10 +1,52 @@
 import { Request, Response } from 'express';
+import { Pass } from '../entities/pass.entity';
+import { Station } from '../entities/station.entity';
+import { Between } from 'typeorm';
 
-export const getTollStationPasses = (req: Request, res: Response) => {
+export const getTollStationPasses = async (req: Request, res: Response) => {
     const { tollStationID, date_from, date_to } = req.params;
+
+    const periodFrom = `${date_from.slice(0, 4)}-${date_from.slice(4, 6)}-${date_from.slice(6, 8)}`;
+    const periodTo = `${date_to.slice(0, 4)}-${date_to.slice(4, 6)}-${date_to.slice(6, 8)}`;
+
+    // Find passes within the date range for the given toll station
+    try{
+            const passes = await Pass.find({
+                where: {
+                    // Filter by station ID
+                    station: { id: tollStationID }, 
+                    timestamp: Between(new Date(periodFrom), new Date(periodTo)), // Filter by date range
+                },
+                //Fetching Pass
+                relations: ['tag', 'station', 'station.operator', 'tag.operator'],
+            });
+
+            const response = {
+                stationID: tollStationID,
+                stationOperator: passes[0]?.station.operator?.name || 'Unknown',
+                requestTimestamp: new Date().toISOString(),
+                periodFrom: date_from,
+                periodTo: date_to,
+                n_passes: passes.length,
+                passList: passes.map((pass, index) =>({
+                    passIndex: index + 1,
+                    passID: pass.id,
+                    timestamp: pass.timestamp.toISOString(),
+                    tagID: pass.tag.id,
+                    tagProvider: pass.tag.operator?.name || 'Unknown',
+                    passType: pass.paid ? "home" : "visitor",
+                    passCharge: pass.charge,
+                }))
+            }
+
+            res.status(200).json(response);
+    } catch {
+        console.error('Error fetching toll station passes:');
+        res.status(500).json({ error: 'Failed to fetch data' });
+    }
     // Placeholder: Implement logic to get toll station passes
-    res.status(200).json({ message: `Fetched passes for ${tollStationID}` });
-}
+    //res.status(200).json({ message: `Fetched passes for ${tollStationID}` });
+};
 
 export const getPassAnalysis = (req: Request, res: Response) => {
     const { stationOpID, tagOpID, date_from, date_to } = req.params;
