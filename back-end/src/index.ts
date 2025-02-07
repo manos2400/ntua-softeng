6ecccac329a90c9867af0062e655117aa4ea5441
Manos Kolydakis <manos1939@gmail.com>
2025-02-07 19:36:45 +0200

    Added healthcheck for the database container and admin creation on the first run

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -2,11 +2,22 @@ import app from './app';
 import { dataSource } from "./config/database";
 import 'reflect-metadata';
 import 'dotenv/config';
+import {User} from "./entities/user.entity";
+import bcrypt from "bcrypt";
 
 const PORT = process.env.PORT || 9115;
 
 // Initialize the database connection
-dataSource.initialize().then(() => {
+dataSource.initialize().then(async () => {
+    // Create an admin user if it doesn't exist
+    if(!(await User.findOneBy({username: 'admin'}))) {
+        const user = new User();
+        user.username = 'admin';
+        user.password = bcrypt.hashSync('freepasses4all', 12);
+        user.isAdmin = true;
+        await user.save();
+        console.log('First run detected. Admin user created');
+    }
     console.log('Database connected');
 }).catch((err) => {
     console.error('Database connection error', err);
